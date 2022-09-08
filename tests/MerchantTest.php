42abee242c5dbaf54391d030e04e0be121b20f8f
xingrufei <xingrufei@didiglobal.com>
2022-09-08 13:58:23 +0800

    add test case

--- tests/MerchantTest.php
+++ tests/MerchantTest.php
@@ -4,24 +4,132 @@ namespace Didipay\Tests;
 
 require_once('src/util/SignUtil.php');
 
+use DidiPay\client\merchantClient;
+use DidiPay\Exceptions\DidipayException;
 use DidiPay\Util\SignUtil;
 use PHPUnit\Framework\TestCase;
 
 class MerchantTest extends TestCase
 {
 
+    const PRI_KEY_PATH = "/home/runner/work/merchant-sdk-php/merchant-sdk-php/app_private_key.pem";
+    const PUB_KEY_PATH = "/home/runner/work/merchant-sdk-php/merchant-sdk-php/app_public_key.pem";
+
     public function test_generate_sign()
     {
         $params = ['merchant_order_id' => '123',
             'pay_order_id' => '123456'];
-        $privateKeyFile = "/home/runner/work/merchant-sdk-php/merchant-sdk-php/app_private_key.pem";
+        $privateKeyFile = self::PRI_KEY_PATH;
         $privateKeyContent = $this->readFile($privateKeyFile);
         echo $privateKeyContent;
         $sign = SignUtil::generateSign($params, $privateKeyContent);
-        $publicKeyFile = "/home/runner/work/merchant-sdk-php/merchant-sdk-php/app_public_key.pem";
+        $publicKeyFile = self::PUB_KEY_PATH;
         $publicKeyContent = $this->readFile($publicKeyFile);
         $isVerify = SignUtil::verifySign($params,$publicKeyContent,$sign);
         $this->assertSame($isVerify,1);
+    }
+
+    /**
+     * @throws DidipayException
+     */
+    public function test_pay_query(){
+
+        $params = ['merchant_order_id' => 'merchantOrderId',
+            'pay_order_id' => 'payOrderId'];
+
+        $privateKeyFile = self::PRI_KEY_PATH;
+        $privateKeyContent = $this->readFile($privateKeyFile);
+        $domain = "https://merchant-server-merchant-sdk-bmtfqitgrw.cn-hangzhou.fcapp.run";
+        $defaultOption = ['app_id' => 'appId', 'merchant_id' => 'merchantId', 'private_key' => $privateKeyContent, 'domain' => $domain];
+
+        $client = new merchantClient($defaultOption);
+        $ret = $client->payQuery($params);
+        $response = json_decode($ret,null,512,1);
+        echo $ret;
+        $this->assertSame('200', $response['returnNo']);
+        $this->assertSame('success', $response['returnMsg']);
+
+    }
+
+    /**
+     * @throws DidipayException
+     */
+    public function test_pre_pay()
+    {
+        $params = ['merchant_order_id' => 'merchantOrderId',
+            'pay_order_id' => 'payOrderId','currency'=>'BRL','total_amount'=>'1200'];
+
+        $privateKeyFile = self::PRI_KEY_PATH;
+        $privateKeyContent = $this->readFile($privateKeyFile);
+        $domain = "https://merchant-server-merchant-sdk-bmtfqitgrw.cn-hangzhou.fcapp.run";
+        $defaultOption = ['app_id' => 'appId', 'merchant_id' => 'merchantId', 'private_key' => $privateKeyContent, 'domain' => $domain];
+
+        $client = new merchantClient($defaultOption);
+        $ret = $client->prePay($params);
+        $response = json_decode($ret,null,512,1);
+        $this->assertSame('200', $response['returnNo']);
+        $this->assertSame('success', $response['returnMsg']);
+    }
+
+    /**
+     * @throws DidipayException
+     */
+    public function test_refund()
+    {
+        // 设置入参$params和秘钥$pkey
+        $params = ['merchant_order_id'=>'merchantOrderId',
+            'pay_order_id'=>'payOrderId','merchant_refund_id'=>'merchantRefundId'];
+
+
+        $privateKeyFile = self::PRI_KEY_PATH;
+        $privateKeyContent = $this->readFile($privateKeyFile);
+        $domain = "https://merchant-server-merchant-sdk-bmtfqitgrw.cn-hangzhou.fcapp.run";
+        $defaultOption = ['app_id' => 'appId', 'merchant_id' => 'merchantId', 'private_key' => $privateKeyContent, 'domain' => $domain];
+
+        $client = new merchantClient($defaultOption);
+        $ret = $client->refund($params);
+        $response = json_decode($ret,null,512,1);
+        $this->assertSame('200', $response['returnNo']);
+        $this->assertSame('success', $response['returnMsg']);
+    }
+
+    /**
+     * @throws DidipayException
+     */
+    public function test_refund_query()
+    {
+        $params = ['merchant_order_id' => 'merchantOrderId',
+            'pay_order_id' => 'payOrderId'];
+
+        $privateKeyFile = self::PRI_KEY_PATH;
+        $privateKeyContent = $this->readFile($privateKeyFile);
+        $domain = "https://merchant-server-merchant-sdk-bmtfqitgrw.cn-hangzhou.fcapp.run";
+        $defaultOption = ['app_id' => 'appId', 'merchant_id' => 'merchantId', 'private_key' => $privateKeyContent, 'domain' => $domain];
+
+        $client = new merchantClient($defaultOption);
+        $ret = $client->refundQuery($params);
+        $response = json_decode($ret,null,512,1);
+        $this->assertSame('200', $response['returnNo']);
+        $this->assertSame('success', $response['returnMsg']);
+    }
+
+    /**
+     * @throws DidipayException
+     */
+    public function test_close_trade()
+    {
+        $params = ['merchant_order_id'=>'merchantOrderId'];
+
+        $privateKeyFile = self::PRI_KEY_PATH;
+        $privateKeyContent = $this->readFile($privateKeyFile);
+        $domain = "https://merchant-server-merchant-sdk-bmtfqitgrw.cn-hangzhou.fcapp.run";
+        $defaultOption = ['app_id' => 'appId', 'merchant_id' => 'merchantId', 'private_key' => $privateKeyContent, 'domain' => $domain];
+
+        $client = new merchantClient($defaultOption);
+        $ret = $client->closeTrade($params);
+        $response = json_decode($ret,null,512,1);
+        $this->assertSame('200', $response['returnNo']);
+        $this->assertSame('success', $response['returnMsg']);
     }
 
     private function readFile($filePath)
@@ -31,7 +139,7 @@ class MerchantTest extends TestCase
             $fp = fopen($filePath, "r");
             $content = "";
             while (!feof($fp)) {
-                $content .= fgets($fp);//逐行读取。如果fgets不写length参数，默认是读取1k。
+                $content .= fgets($fp);
             }
             fclose($fp);
             return $content;
